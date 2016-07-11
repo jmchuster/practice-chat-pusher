803a0950ec7c03dd5063406643f6a02ad48883de
Jonathan Chu <jmchuster@gmail.com>
2016-07-10 18:02:55 -0700

    post messages to pusher

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,6 +11,7 @@ import hello.views
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
+    url(r'^chat', hello.views.chat, name='chat'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_http_methods
+from pusher import Pusher
+import os
 
 from .models import Greeting
+
+class JsonStatus:
+    def Ok(message = '', data = {}):
+        return JsonResponse({**data, **{'status': 'OK', 'message': message}})
+
+    def Noop(message = '', data = {}):
+        return JsonResponse({**data, **{'status': 'NOOP', 'message': message}})
+
+    def Error(message = '', data = {}):
+        return JsonResponse({**data, **{'status': 'ERROR', 'message': message}})
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+@require_http_methods(['GET', 'POST'])
+def chat(request):
+    if request.method == 'GET':
+        return render(request, 'chat.html')
+    elif request.method == 'POST':
+        import pdb; pdb.set_trace()
+        message = request.POST.get('message', '').strip()
+        if len(message) == 0:
+            return JsonStatus.Noop()
+
+        pusher = Pusher(
+          app_id = os.environ.get('PUSHER_APP_ID'),
+          key = os.environ.get('PUSHER_APP_KEY'),
+          secret = os.environ.get('PUSHER_APP_SECRET')
+        )
+
+        pusher.trigger('chat_channel', 'message_event', {'message': message})
+        return JsonStatus.Ok()
 
 def db(request):
 
@@ -17,4 +48,3 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-
